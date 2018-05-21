6d15d7b209006b4d505ead0daa1c6b914ee479c9
TiagoFuelber <tfuelber@gmail.com>
2018-05-21 20:03:03 -0300

    finshes first class of module 4

--- public/app/utils/array-helpers.js
+++ public/app/utils/array-helpers.js
@@ -1,12 +1,5 @@
 if (!Array.prototype.$flatMap) {
     Array.prototype.$flatMap = function(cb) {
-<<<<<<< HEAD
         return this.map(cb).reduce((destArray, array) => destArray.concat(array), [])
     };
 };
-=======
-        return this.map(cb).reduce((destArray, array) => 
-            destArray.concat(array), []);
-    }
-}
->>>>>>> ecaae6f08219034ff63589ca4942eb42a514a8bb
--- public/app/utils/operators.js
+++ public/app/utils/operators.js
@@ -2,11 +2,10 @@ export const partialize = (fn, ...args) =>
     fn.bind(null, ...args);
 
 export const compose = (...fns) => value => 
-<<<<<<< HEAD
     fns.reduceRight((previousValue, fn) => fn(previousValue), value)
 
 export const pipe = (...fns) => value =>
     fns.reduce((previousValue, fn) => fn(previousValue), value)
-=======
-    fns.reduceRight((previousValue, fn) => fn(previousValue), value);
->>>>>>> ecaae6f08219034ff63589ca4942eb42a514a8bb
+
+export const takeUntil = (times, fn) => 
+    () => times-- > 0 && fn(); 
