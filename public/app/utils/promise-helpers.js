48209215a0ba8ea4035e43615a184df04c01e88f
TiagoFuelber <tfuelber@gmail.com>
2018-05-28 20:39:47 -0300

    implements timeout in promises

--- public/app/utils/promise-helpers.js
+++ public/app/utils/promise-helpers.js
@@ -4,3 +4,10 @@ export const log = param => {
     console.log(param);
     return param;
 }   
+
+export const timeoutPromise = (miliiseconds, promise) => {
+    const timeout = new Promise((resolve, reject) => 
+        setTimeout(() => reject(`Limite da promise excedido (limite: ${miliiseconds} ms)`), miliiseconds))
+
+    return Promise.race([timeout, promise]);
+}
